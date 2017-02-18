328bef066cb66b61f8f2a2a9d4acfdea5e30aaca
Jack Howerton <jackhowerton@gmail.com>
2017-02-18 04:00:26 -0800

    cleanup format

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -3,7 +3,7 @@ get '/rounds/:round_id/cards/last' do
 	@last_card = @round.deck.cards.last
 	@previous_user_answer = params[:answer]
 
-	if @previous_user_answer == @last_card.answer
+	if @previous_user_answer.upcase == @last_card.answer.upcase
 		Guess.create( selection: "True", card_id: @last_card.id, user_id: @round.user.id, round_id: @round.id)
 	else
 		Guess.create( selection: "False", card_id: @last_card.id, user_id: @round.user.id, round_id: @round.id)
@@ -22,14 +22,11 @@ get '/rounds/:round_id/cards/:id' do
 
 	@previous_card = nil
 	@last_card = @round.deck.cards.where([ "id > ?",@card.id]).last
-
 	@previous_card = @round.deck.cards.where([ "id < ?",@card.id]).last if @card.id > @round.deck.cards.first.id
-	
-
 
 	if @card.id > @round.deck.cards.first.id
 		@previous_user_answer = params[:answer]
-		if @previous_user_answer == @previous_card.answer
+		if @previous_user_answer.upcase == @previous_card.answer.upcase
 			Guess.create( selection: "True", card_id: @card.id, user_id: @round.user.id, round_id: @round.id)
 		else
 			Guess.create( selection: "False", card_id: @card.id, user_id: @round.user.id, round_id: @round.id)
